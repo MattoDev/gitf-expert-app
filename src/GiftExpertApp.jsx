63e94109aa37a37d3ae1d3dfaef2b9d375b80265
MattoDev <mateofonseca.profesional@gmail.com>
2022-12-06 21:58:34 -0500

    css style

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
   //RGHLs0xVKnCl5TX5DCkuvLH2LW5WqvtZ
+  // No debo poner condicionalmente mis hooks
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  console.log(categories);
   return (
     <>
       <h1>GiftExpertApp</h1>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
