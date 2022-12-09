477df3b90fdca882571c8c0c9cd1878d31def2c4
MattoDev <mateofonseca.profesional@gmail.com>
2022-12-09 07:24:52 -0500

    unique key

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -7,6 +7,7 @@ export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories((current) => [...current, newCategory]);
   };
 
