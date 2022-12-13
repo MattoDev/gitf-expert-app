f1fe9dae75c915de53ab96f68615fc2fe22bdf7b
MattoDev <mateofonseca.profesional@gmail.com>
2022-12-12 22:28:07 -0500

    useEffect getGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  // el useEffect es un kook de React que sirve para ejecutar efectos secundarios(proceso que quiera ejecutar cuando algo suceda)
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
