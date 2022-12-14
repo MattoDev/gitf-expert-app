06897f9bdf186f2d5a4986349f6598aa22282b26
MattoDev <mateofonseca.profesional@gmail.com>
2022-12-13 22:52:15 -0500

    Index File

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
 
 export const GiftExpertApp = () => {
   //RGHLs0xVKnCl5TX5DCkuvLH2LW5WqvtZ
